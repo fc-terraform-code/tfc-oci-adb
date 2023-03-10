da71635aa158761af001fb8039e63c116dbc4c4e
facundocayon <facundocayon@gmail.com>
2023-03-10 16:36:47 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,7 @@
-## Copyright (c) 2021 Oracle and/or its affiliates.
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-output "adb_database" {
-  value = {
-    adb_database_id     = oci_database_autonomous_database.adb_database.id
-    connection_urls     = oci_database_autonomous_database.adb_database.connection_urls
-    adb_wallet_content  = oci_database_autonomous_database_wallet.adb_database_wallet.content
-    adb_nsg_id          = (!var.use_existing_vcn && var.adb_private_endpoint) ? oci_core_network_security_group.adb_nsg[0].id : var.adb_nsg_id
-    private_endpoint_ip = var.adb_private_endpoint ? join(",", oci_database_autonomous_database.adb_database[*].private_endpoint_ip) : ""
-  }
+output "db_name" {
+  value = oci_database_autonomous_database.tf_adb.display_name
 }
 
-
-
+output "db_state" {
+  value = oci_database_autonomous_database.tf_adb.state
+}
