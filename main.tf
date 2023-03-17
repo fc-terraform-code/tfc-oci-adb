4c84d31d376bf7b908ee185dfdaa0544d5b9aaf6
facundocayon <facundocayon@gmail.com>
2023-03-17 10:16:15 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,5 +16,4 @@ resource "oci_database_autonomous_database" "tf_adb" {
   is_free_tier             = var.is_free_tier
   license_model            = var.license_model
   freeform_tags            = var.freeform_tags
-  defined_tags             = var.defined_tags
 }
